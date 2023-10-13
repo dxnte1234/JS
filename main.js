d959bfd97fd33bbb22a08c9b9cb2d32e48756b9e
dxnte1234 <danksolco@hotmail.com>
2023-10-13 03:24:24 -0600

    First excercise

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hola Koders"); 
+
+let a = 5   
+let b = 7|
+
+let result1 = (a + b);
+let result2 = (a - b);
+let result3 = (a / b);
+let result4 = (a % b);
+let result5 = (a * b);
+
+console.log(result1);
+console.log(result3);
+console.log(result2);
+console.log(result4);
+console.log(result5);
+
+
+
